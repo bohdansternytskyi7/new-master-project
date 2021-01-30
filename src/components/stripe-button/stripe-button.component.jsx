cbd9be9389cfa281c2a5a968155ca055e3ea7bf2
bohdan <bohdansternytskyi@gmail.com>
2021-01-30 22:44:42 +0100

    adding stripe test payment, added favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51IFMwcDMXTIlS14NJvPYf2hsLjS7bQ0pAegU4bWQvDT1Wqb7s0laWND0BC2LPiWDdvoNYrLBFv33KiWfxajhP1HD00zvLEJEG1';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
